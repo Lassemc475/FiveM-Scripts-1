1a5852624267ebb07d08d5b1c6aea1fb4c71930e
Lassemc475 <Lassemc123@gmail.com>
2019-12-23 17:34:27 +0100

    Blacklist

--- kasperr_jobcenter/server/main.lua
+++ kasperr_jobcenter/server/main.lua
@@ -1,16 +1,19 @@
 --[[
-  __  __           _            _                 _  __                         _____  
- |  \/  |         | |          | |               | |/ /                        |  __ \ 
+  __  __           _            _                 _  __                         _____
+ |  \/  |         | |          | |               | |/ /                        |  __ \
  | \  / | __ _  __| | ___      | |__  _   _      | ' / __ _ ___ _ __   ___ _ __| |__) |
- | |\/| |/ _` |/ _` |/ _ \     | '_ \| | | |     |  < / _` / __| '_ \ / _ \ '__|  _  / 
- | |  | | (_| | (_| |  __/     | |_) | |_| |     | . \ (_| \__ \ |_) |  __/ |  | | \ \ 
+ | |\/| |/ _` |/ _` |/ _ \     | '_ \| | | |     |  < / _` / __| '_ \ / _ \ '__|  _  /
+ | |  | | (_| | (_| |  __/     | |_) | |_| |     | . \ (_| \__ \ |_) |  __/ |  | | \ \
  |_|  |_|\__,_|\__,_|\___|     |_.__/ \__, |     |_|\_\__,_|___/ .__/ \___|_|  |_|  \_\
-                                       __/ |                   | |                     
-                                      |___/                    |_|                     
+                                       __/ |                   | |
+                                      |___/                    |_|
 
   Author: Kasper Rasmussen
   Steam: https://steamcommunity.com/id/kasperrasmussen
 ]]
+
+local blacklist = {"staff", "ejer", "admin"}
+local blacklisted = false
 
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
@@ -20,7 +23,20 @@ vRPclient = Tunnel.getInterface("vRP", "kasperr_jobcenter")
 
 RegisterServerEvent("kasperr_jobcenter:selectJob")
 AddEventHandler("kasperr_jobcenter:selectJob", function(group)
+
+  for k,v in pairs(blacklist) do
+    if (group == v) then
+    -- body...
+    blacklisted = true
+    end
+  end
+  if blacklisted = false then
     local user_id = vRP.getUserId({source})
     vRP.addUserGroup({user_id,group})
+    print("Ikke bugger")
     TriggerClientEvent("kasperr_jobcenter:notification", source, Config.Language.SelectedJob .. " " .. group)
+  else
+    print("Bugger")
+    TriggerClientEvent("kasperr_jobcenter:notification", source, "Nej. Stop med det der")
+  end
 end)
