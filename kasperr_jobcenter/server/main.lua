c49f0a4f6ca7438b83808af2c82daa9483525448
Lassemc475 <Lassemc123@gmail.com>
2019-12-23 17:51:26 +0100

    Update main.lua

--- kasperr_jobcenter/server/main.lua
+++ kasperr_jobcenter/server/main.lua
@@ -33,11 +33,9 @@ AddEventHandler("kasperr_jobcenter:selectJob", function(group)
   if blacklisted == false then
     local user_id = vRP.getUserId({source})
     vRP.addUserGroup({user_id,group})
-    print("Ikke bugger")
     TriggerClientEvent("kasperr_jobcenter:notification", source, Config.Language.SelectedJob .. " " .. group)
     blacklisted = false
   else
-    print("Bugger")
     TriggerClientEvent("kasperr_jobcenter:notification", source, "Nej. Stop med det der")
     blacklisted = false
   end
